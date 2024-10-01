b86c69d220d3e8189bb998f88b3154c0b5d05de1
AhmadYosef2712 <ahijazi083@gmail.com>
2024-10-01 15:15:17 +0300

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textV;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textV=findViewById(R.id.textView);
+
+
+    }
+
+    public void changeT(View view) {
+        textV.setText("This is my first app");
+    }
+
+    public void leave(View view) {
+        finish();
+        System.exit( 0);
+    }
+}
